fdb86f2bc7277addfb2939f5969d9a780753c101
Tervel <jordananslow@gmail.com>
2016-08-14 05:23:42 +1000

    Add a working csv to json script and update .gitignore

--- tools/ipToGeoLocation.py
+++ tools/ipToGeoLocation.py
@@ -1,21 +1,34 @@
-import subprocess
+import requests
+import json
 import csv
 
-outlist = []
 
-with open("blacklist.csv", "rb") as csvFile:
-    reader = csv.reader(csvFile, delimiter="\t")
-    count = enumerate(reader)
-    for line in count:
-        outlist.append(line[1][0])
+def read_file(file):
+    ip_addresses = []
+    with open(file, 'r') as csvFile:
+        reader = csv.reader(csvFile, delimiter='\t')
+        count = enumerate(reader)
+        for line in count:
+            ip_addresses.append(line[1][0])
 
-with open("blacklistGeo.csv", "wb") as writeFile:
-    writer = csv.writer(writeFile, delimiter=',')
-    for line in outlist:
-        output = subprocess.check_output(["curl", "ipinfo.io/{}/loc".format(line)])
-        output = output[:-1]
-        writer.writerow([line, output])
+    return ip_addresses
 
+def get_details(ip_addresses):
+    ip_address_details = []
 
+    for address in ip_addresses:
+        request = requests.get('http://ip-api.com/json/{}'.format(address))
+        ip_address_details.append(request.json())
 
+    return ip_address_details
+
+def main():
+    ip_addresses = read_file('blacklist.csv')
+    ip_addresses_json = get_details(ip_addresses)
+
+    with open('blacklist.json', 'w') as outfile:
+        json.dump(ip_addresses_json, outfile, sort_keys = True, indent = 4)
+
+if __name__ == "__main__":
+    main()
 
